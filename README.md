3d3d9a734d60e655574b0c52a6858170cf390390
Daniel Golbig <daniel@golbig.de>
2014-04-01 23:49:34 +0200

    initial commit, first functional version

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-author_gravatar
-===============
+# Author Gravatar
+-----------------
+
+Version: 1.0  
+Author: Daniel Golbig (daniel@mediamadness.de)
+
+
+## About
+
+Adds the logged in authors gravatar to the admin page head
+
+
+## Installation
+
+1. Upload the `author_gravatar` folder in this archive to your Symphony `extensions` folder.
+2. Enable it the usual way.
+
+
+## Changelog
+
+### 1.0
+ - Initial release
+
+
+## ToDo's
+
+1. Maybe add context menu to the gravatar for profile management -> maybe even put the *Log Out* menu item in there as well
---
+++ extension.driver.php
@@ -0,0 +1,76 @@
+<?php
+
+	if(!defined("__IN_SYMPHONY__")) die("<h2>Error</h2><p>You cannot directly access this file</p>");
+
+	Class extension_author_gravatar extends Extension {
+	
+		public function getSubscribedDelegates() {
+			return array(
+				array(
+					'page'		=> '/backend/',
+					'delegate'	=> 'InitaliseAdminPageHead',
+					'callback'	=> 'appendAssets'
+				)
+			);
+		}
+	
+		public function appendAssets($context) {
+
+			// add stylesheet with changes to header
+			Administration::instance()->Page->addStylesheetToHead(URL . '/extensions/author_gravatar/assets/author_gravatar.admin.css', 'screen', 100);
+
+			// create gravatar image
+			$img = new XMLElement('img', null, array(
+				'src' => $this->getGravatar($this->getAuthorEmail(), 30), 
+				'class' => 'gravatar'
+			));
+
+			// create anchor element as parent to the gravatar image
+			$a = new XMLElement('a', $img, array(
+				'href' => SYMPHONY_URL . '/system/authors/edit/' . Administration::instance()->Author->get('id') . '/',
+				'data-id' => Administration::instance()->Author->get('id'),
+				'data-name' => Administration::instance()->Author->get('first_name'),
+				'data-type' => Administration::instance()->Author->get('user_type')
+			));
+			
+			// append anchor / gravatar image to backend header element
+			Administration::instance()->Page->Header->appendChild($a);
+		}
+
+		/**
+		 * Get the current authors email address
+		 * @return String containing the email address or empty string
+		 */
+		public static function getAuthorEmail() {
+			$author = Administration::instance()->Author;
+			if (isset($author)) { 
+				return $author->get('email'); 
+			} else { 
+				return ''; 
+			}
+		}
+
+		/**
+		 * Get either a Gravatar URL or complete image tag for a specified email address.
+		 * @param string $email The email address
+		 * @param string $s Size in pixels, defaults to 80px [ 1 - 2048 ]
+		 * @param string $d Default imageset to use [ 404 | mm | identicon | monsterid | wavatar ]
+		 * @param string $r Maximum rating (inclusive) [ g | pg | r | x ]
+		 * @param boole $img True to return a complete IMG tag False for just the URL
+		 * @param array $atts Optional, additional key/value attributes to include in the IMG tag
+		 * @return String containing either just a URL or a complete image tag
+		 */
+		public static function getGravatar( $email, $s = 80, $d = 'mm', $r = 'g', $img = false, $atts = array() ) {
+		    $url = 'http://www.gravatar.com/avatar/';
+		    $url .= md5( strtolower( trim( $email ) ) );
+		    $url .= "?s=$s&d=$d&r=$r";
+		    if ( $img ) {
+		        $url = '<img src="' . $url . '"';
+		        foreach ( $atts as $key => $val )
+		            $url .= ' ' . $key . '="' . $val . '"';
+		        $url .= ' />';
+		    }
+		    return $url;
+		}
+	
+	}
